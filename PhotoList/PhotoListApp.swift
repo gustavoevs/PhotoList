e1d417b4326bc032488dba9773926f596bbbbbfd
gustavoevs <queondatavo@gmail.com>
2022-09-01 12:18:59 +0200

    Initial Commit

---
+++ PhotoList/PhotoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoListApp.swift
+//  PhotoList
+//
+//  Created by Gustavo Verdugo on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
